7dac68f6373add5db2ff5a672bc9e35c0c046ad9
semmozhiyan-dev <semmozhi12122005@gmail.com>
2026-06-10 14:54:25 +0530

    validation schema

--- src/utils/validationSchema.mjs
+++ src/utils/validationSchema.mjs
@@ -1,4 +1,21 @@
 export const createUserValidationSchema ={
-    
+    user_name:{
+        notEmpty:{
+            errorMessage:"user name must not be empty"
+        },
+        isLength:{
+            options:{min:3,max:12},
+            errorMessage:"user name length not required"
+        },
+        isString:{
+            errorMessage:"user name must be string"
+        }
+    },
+    age:{
+        notEmpty:{
+            errorMessage:" age must not be empty"
+        }
+
+    }
 
 }
